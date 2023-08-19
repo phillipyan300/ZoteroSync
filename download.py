f2dfd2d885a3c4c1a48bcbaf343898b0a474172c
Phillip Yan <phillip.m.yan@gmail.com>
2023-08-19 12:02:08 -0400

    Refactored download and upload and added helper methods

--- download.py
+++ download.py
@@ -4,162 +4,156 @@ import os
 import subprocess
 
 #How to stop it from accessing trash
-
-def main():
-
-    #These three things I need to get from the user
-    userID = ""
-    api_key = ""
-    directory_name = "ZoteroImports"
-    full_directory_path = ""
-
-    #Dictionary where the PDF key is the key and the value is another dictionary which contains the relevant information for the dictionary
-    PDFDictionary = {}
-    makeFolder("ZoteroImports", full_directory_path)
-    extract(userID, api_key, PDFDictionary, full_directory_path)
-
-    doneViewing = False
-    while not doneViewing:
-        doneViewing = openPDF(PDFDictionary, directory_name)
-
-#Multiple API calls needed bc limits on each + need to do pdfs first then annotations
-def extract(userID: str, api_key: str, PDFDictionary: dict, full_directory_path: str) -> None:
-    start = 0
-    itemsLeft = True
-    while itemsLeft:
-        itemsLeft = getPDFS(userID, api_key, PDFDictionary, full_directory_path, start)
-        start += 25
-
-    # Annotations
-    start = 0
-    itemsLeft = True
-    while itemsLeft:
-        itemsLeft = getAnnotations(userID, api_key, PDFDictionary, full_directory_path, start)
-        start += 25
-
-def makeFolder(directory_name: str, full_directory_path: str) -> None:
-    if not os.path.exists(full_directory_path):
-        os.mkdir(directory_name)
-
-#Generic API get function, with generic error checking
-def APIrequest(link: str, api_key: str) -> requests.models.Response:
-    headers = {
-        "Zotero-API-Version": "3",
-        "Zotero-API-Key": api_key,
-
-    }
-    try:
-        response = requests.get(link, headers = headers)
-        if response.status_code == 200:
-            return response
-        else:
-            print(f"API call failed. Status code: {response.status_code}. ")
-            exit()
-    except Exception as e:
-       print(f"API request error: {e}")
-
-
-def openPDF(PDFDictionary: dict, directoryname: str) -> bool:
-    print(f"\nYour Zotero library has the following files: ")
-    counter = 0
-    for key, value in PDFDictionary.items():
-        counter += 1
-        print(f"{counter}. {value['pdf_name']}")
-        #Assign an ordering to the Dictionary
-        PDFDictionary[key]["Order"] = counter
-
-    fileno = int(input("Enter -1 to quit. Choose a file to open by its number: (i.e. 1)  "))-1
-
-    if fileno == -2:
-        return True
-    elif fileno >= len(PDFDictionary) or fileno < 0:
-        print("Not a valid number!")
-        return False
-    else:
-        for key, value in PDFDictionary.items():
-            if value["Order"] == (fileno + 1):
-                subprocess.run(["open", f"{directoryname}/{PDFDictionary[key]['pdf_name']}"])
-        return False
-
 #TODO: When the user specifies a specific directory, wll need to change the os.path.join
-def getPDFS(userID: str, api_key: str, PDFDictionary: dict, full_directory_path: str, start: int) -> bool:
-    response = APIrequest(f"https://api.zotero.org/users/{userID}/items?limit=25&start={start}", api_key)
+#Maybe add some way to access webpage screenshots too, beyond just PDFs
+#Permanent storage of PDF Dictionary? even after program has stopped running? maybe a text file?
 
-    items = response.json()
+class downloadZoteroAPI:
+    def __init__(self, userID: str, api_key: str, directory_name: str):
+        self.userID = userID
+        self.api_key = api_key
+        self.directory_name = directory_name
+        self.full_directory_path = os.path.join(os.getcwd(), directory_name)
+        self.PDFDictionary = {}
 
-    #Commented code below lists all json data extracted from all the items of the library
-    #print(json.dumps(items, indent=4))
-    # If there are no more items, return false.
-    if not len(items):
-        return False
+    def makeFolder(self):
+        if not os.path.exists(self.full_directory_path):
+            os.mkdir(self.directory_name)
 
-    # Each item is an item in Zotero represented as a dictionary. Beware of duplicates, a single file will have both a journal article (containing metadata) and a pdf
-    for item in items:
-        # Attachments
-        if item["data"]["itemType"] == "attachment":
-            pdf_name = item["data"]["title"]
-            print(f"Exporting PDF: {pdf_name}")
-            pdf_key = item["data"]["key"]
-            pdf_url = f"https://api.zotero.org/users/{userID}/items/{item['data']['key']}/file/view"
-            #pdf_url = item["links"]["enclosure"]["href"]
+    # Multiple API calls needed because limit on number of files per API call
+    def extract(self):
+        start = 0
+        itemsLeft = True
+        while itemsLeft:
+            itemsLeft = self.getPDFS(start)
+            start += 25
 
-            # Get the PDF and store it in the drive
-            response = APIrequest(pdf_url, api_key)
-            file_path = os.path.join(full_directory_path, pdf_name)
-            with open(file_path, "wb") as f:
-                f.write(response.content)
-
-            #Add to the dictionary of pdfs
-            PDFDictionary[pdf_key] = {"pdf_name": pdf_name, "pdf_url": pdf_url}
-    return True
-
-#TODO: Redo the same repetition as for getPDFS
-def getAnnotations(userID: str, api_key: str, PDFDictionary: dict, full_directory_path: str, start: int) -> bool:
-    response = APIrequest(f"https://api.zotero.org/users/{userID}/items?limit=25&start={start}", api_key)
-
-    items = response.json()
-    if not len(items):
-        return False
-
-
-    for item in items:
         # Annotations
-        if item["data"]["itemType"] == "annotation":
-            annotation_title = item["key"]
-            print(f"Exporting annotation: {annotation_title}")
-            annotation_url = item["links"]["self"]["href"]
-            annotation_parent_key = item["data"]["parentItem"]
+        start = 0
+        itemsLeft = True
+        while itemsLeft:
+            itemsLeft = self.getAnnotations(start)
+            start += 25
 
+    def getPDFS(self, start: int) -> bool:
+        response = self.getAPIrequest(f"https://api.zotero.org/users/{self.userID}/items?limit=25&start={start}")
 
-            #Get Annotation and store in local repo
-            response = APIrequest(annotation_url, api_key)
-            file_path = os.path.join(full_directory_path, annotation_title)
-            with open(file_path, "wb") as f:
-                f.write(response.content)
-            #Add to dictionary
-            PDFDictionary[annotation_parent_key]["annotation_url"] = annotation_url
+        items = response.json()
 
+        # Commented code below lists all json data extracted from all the items of the library
+        # print(json.dumps(items, indent=4))
+        # If there are no more items, return false.
+        if not len(items):
+            return False
 
+        # Each item is an item in Zotero represented as a dictionary. Beware of duplicates, a single file will have both a journal article (containing metadata) and a pdf
+        for item in items:
+            # Attachments
+            if item["data"]["itemType"] == "attachment":
+                pdf_name = item["data"]["title"]
+                print(f"Exporting PDF: {pdf_name}")
+                pdf_key = item["data"]["key"]
+                pdf_url = f"https://api.zotero.org/users/{self.userID}/items/{item['data']['key']}/file/view"
+                # Get the PDF and store it in the drive
+                response = self.getAPIrequest(pdf_url)
+                file_path = os.path.join(self.full_directory_path, pdf_name)
+                with open(file_path, "wb") as f:
+                    f.write(response.content)
 
-            #TODO call antother function to clip the annotation on to the parent function, pikepdf?
+                # Add to the dictionary of pdfs
+                self.PDFDictionary[pdf_key] = {"pdf_name": pdf_name, "pdf_url": pdf_url}
+        return True
 
+    # Helper Method for extract
+    def getAnnotations(self, start: int) -> bool:
+        response = self.getAPIrequest(f"https://api.zotero.org/users/{self.userID}/items?limit=25&start={start}")
 
+        items = response.json()
+        if not len(items):
+            return False
 
+        for item in items:
+            # Annotations
+            if item["data"]["itemType"] == "annotation":
+                annotation_title = item["key"]
+                print(f"Exporting annotation: {annotation_title}")
+                annotation_url = item["links"]["self"]["href"]
+                annotation_parent_key = item["data"]["parentItem"]
 
+                # Get Annotation and store in local repo
+                response = self.getAPIrequest(annotation_url)
+                file_path = os.path.join(self.full_directory_path, annotation_title)
+                with open(file_path, "wb") as f:
+                    f.write(response.content)
+                # Add to dictionary
+                self.PDFDictionary[annotation_parent_key]["annotation_url"] = annotation_url
+
+                # TODO call another function to clip the annotation on to the parent function, pikepdf?
+
+    # Generic API get function, with generic error checking
+    def getAPIrequest(self, link: str) -> requests.models.Response:
+        headers = {
+            "Zotero-API-Version": "3",
+            "Zotero-API-Key": self.api_key,
+
+        }
+        try:
+            response = requests.get(link, headers=headers)
+            if response.status_code == 200:
+                return response
+            else:
+                print(f"API call failed. Status code: {response.status_code}. ")
+                exit()
+        except Exception as e:
+            print(f"API request error: {e}")
+
+    def openPDF(self) -> bool:
+        print(f"\nYour Zotero library has the following files: ")
+        counter = 0
+        for key, value in self.PDFDictionary.items():
+            counter += 1
+            print(f"{counter}. {value['pdf_name']}")
+            # Assign an ordering to the Dictionary
+            self.PDFDictionary[key]["Order"] = counter
+
+        fileno = int(input("Enter -1 to quit. Choose a file to open by its number: (i.e. 1)  ")) - 1
+
+        if fileno == -2:
+            return True
+        elif fileno >= len(self.PDFDictionary) or fileno < 0:
+            print("Not a valid number!")
+            return False
+        else:
+            for key, value in self.PDFDictionary.items():
+                if value["Order"] == (fileno + 1):
+                    subprocess.run(["open", f"{self.directory_name}/{self.PDFDictionary[key]['pdf_name']}"])
+            return False
+
+    def savePDFLibraryDict(self):
+        with open("PDFDictionary.json", "w") as file:
+            json.dump(self.PDFDictionary, file)
+
+    def download(self):
+        self.makeFolder()
+        self.extract()
+        self.savePDFLibraryDict()
+        doneViewing = False
+        while not doneViewing:
+            doneViewing = self.openPDF()
+
+    #Data persistence for uploads
+
+    def clearPDFLibraryDict(self):
+        with open("PDFDictionary.json", "w") as file:
+            pass
 
 
 if __name__ == "__main__":
-    main()
+    zotero = downloadZoteroAPI(userID = "", api_key = "", directory_name = "")
+    zotero.download()
 
 
 
-# JournalArticle - Like a wrapper class (Not sure if this is useful, maybe we might need the metadata?)
-        #Lets comment this out for now, it just clogs up the run time
-        # elif item["data"]["itemType"] == "journalArticle":
-        #     wrapper_title = item["data"]["title"]
-        #     wrapper_url = item["links"]["attachment"]["href"]
-        #     print(f"Exporting journalArticle wrapper: {wrapper_title}")
-        #     response = APIrequest(wrapper_url, api_key)
-        #     file_path = os.path.join(full_directory_path, wrapper_title)
-        #     with open(file_path, "wb") as f:
-        #         f.write(response.content)
+
+
+
--- upload.py
+++ upload.py
@@ -4,125 +4,162 @@ import os
 import subprocess
 import hashlib
 
-#Notes: This is uploading a new file, but I can theoretically update/edit and old file with the partial file upload
-#Need to build the above functionality, two potential paths, depending on the user choiceg
-userID = ""
-api_key = ""
 
-#Start by just pushing pdfs.
-headers = {
-        "Zotero-API-Version": "3",
-        "Zotero-API-Key": api_key,
+#Uploading one file at a time
 
-    }
-response = requests.get(f"https://api.zotero.org/items/new?itemType=attachment&linkMode=imported_file", headers = headers)
-print(json.dumps(response.json(), indent=4))
-structure = response.json()
-#Applying an arbitrary pdf
-structure["title"] = "AApaper.pdf"
-structure["filename"] = "AApaper.pdf"
-structure["contentType"] = "application/pdf"
-print(json.dumps(structure, indent=4))
+#FIlename vs title?
 
 
-#Now step 2, actually filling this out and obtaining a itemkey
-headers = {
-    'Content-Type': 'application/json',
-    "Zotero-API-Version": "3",
-    "Zotero-API-Key": api_key,
-}
-response = requests.post(f"https://api.zotero.org/users/{userID}/items", json=[structure], headers=headers)
-print(response)
-print("step two done")
-print(json.dumps(response.json(), indent=4))
-jsonToGetKey = response.json()
+#changing the name of a file doesn't change its md5 hash (which is what Zotero uses to identify it.)
+#Make it so that when I upload, I also update the PDFDIctionary with the file just uploaded.
+class uploadZoteroAPI:
+
+    def __init__(self, userID: str, api_key: str, directory_address: str, file_name: str):
+        self.userID = userID
+        self.api_key = api_key
+        self.directory_address = directory_address
+        self.filename = file_name
+        self.full_directory_path = os.path.join(os.getcwd(), directory_address)
+
+        with open("PDFDictionary.json", "r") as file:
+            self.PDFDictionary = json.load(file)
+
+    def upload(self):
+        #First check if the file exists; if so, then update the file rather than uploading a completely new one
+        pdf_key = None
+        for key, value in self.PDFDictionary.items():
+            if value["pdf_name"] == self.filename:
+                pdf_key = key
+        if pdf_key:
+            print("uploading Existing")
+            self.uploadExisting(pdf_key, self.filename)
+        else:
+            print("uploading New")
+            self.uploadNew(self.filename)
+
+    def uploadNew(self, filename: str):
+        # Step 1: Obtain the Template and edit it
+        response = self.getAPIrequest("https://api.zotero.org/items/new?itemType=attachment&linkMode=imported_file")
+        structure = response.json()
+        structure["title"] = filename
+        structure["filename"] = filename
+        structure["contentType"] = "application/pdf"
+
+        # Step 2: Obtaining an itemkey
+        headers = {
+            'Content-Type': 'application/json',
+            "Zotero-API-Version": "3",
+            "Zotero-API-Key": self.api_key,
+        }
+        response = requests.post(f"https://api.zotero.org/users/{self.userID}/items", json=[structure], headers=headers)
+        print("\n\nthe skelenton from step 2: "+ json.dumps(response.json(), indent=4) + "\n\n")
+        jsonToGetKey = response.json()
+
+        # Step 3: Upload confirmation and get md5, mtime, and filesize
+
+        mtime = os.path.getmtime(f"{self.directory_address}{self.filename}") * 1000  # Convert to milliseconds
+        md5_hash = hashlib.md5()
+        with open(f"{self.directory_address}{self.filename}", "rb") as file:
+            for byte_block in iter(lambda: file.read(4096), b""):
+                md5_hash.update(byte_block)
+        md5 = md5_hash.hexdigest()
+
+        filesize = os.path.getsize(f"{self.directory_address}{self.filename}")
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Zotero-API-Version": "3",
+            "Zotero-API-Key": self.api_key,
+            "If-None-Match": "*"
+        }
+
+        data = {
+            "md5": md5,
+            "filename": self.filename,
+            "filesize": filesize,
+            "mtime": mtime
+        }
+
+        #basically telling them what the key will be
+        response = requests.post(f"https://api.zotero.org/users/{self.userID}/items/{jsonToGetKey['successful']['0']['key']}/file", data=data, headers=headers)
+
+        print(response)
+        print(json.dumps(response.json(), indent=4))
+
+        print("step three done")  # The step to confirm upload ability
 
 
-#Now step 3, confirming that I can upload
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Zotero-API-Version": "3",
-    "Zotero-API-Key": api_key,
-    "If-None-Match": "*"
-}
+        # Step 4: Uploading the file
+        headers = {
+            "Content-Type": response.json()['contentType'],  # API response from step 3 needs to not say exists or this will break
+            "Zotero-API-Version": "3",
+            "Zotero-API-Key": self.api_key
+        }
 
-#I don't think I need this? Mtime and md5? NOt sure,gonna keep it cuz chatGPT said so lol
-mtime = os.path.getmtime(f"/Users/phillipyan/Downloads/{jsonToGetKey['successful']['0']['data']['title']}") * 1000  # Convert to milliseconds
-filename = "AApaper.pdf"
-md5_hash = hashlib.md5()
-with open(f"/Users/phillipyan/Downloads/{jsonToGetKey['successful']['0']['data']['title']}", "rb") as file:
-    for byte_block in iter(lambda: file.read(4096), b""):
-        md5_hash.update(byte_block)
-md5 = md5_hash.hexdigest()
+        # Concatenate prefix, file content, and suffix
+        with open(f"{self.directory_address}{jsonToGetKey['successful']['0']['data']['title']}", "rb") as file:
+            file_content = file.read()
 
-data = {
-    "md5": md5,  # The MD5 hash of the file
-    "filename": jsonToGetKey['successful']['0']['data']['filename'],
-    "filesize": 626851,  #Will need to find a way to get this number automatically
-    "mtime": mtime  # The modification time in milliseconds
-}
+        # Convert prefix and suffix to bytes
+        prefix_bytes = response.json()['prefix'].encode('utf-8')
+        suffix_bytes = response.json()['suffix'].encode('utf-8')
 
-#This should get the key for the item
-print(jsonToGetKey['successful']['0']['key'])
-response = requests.post(f"https://api.zotero.org/users/{userID}/items/{jsonToGetKey['successful']['0']['key']}/file", data=data, headers=headers)
+        # Now concatenate
+        payload = prefix_bytes + file_content + suffix_bytes
 
-print(response)
-print(response.text)
-print(json.dumps(response.json(), indent=4))
+        # Upload the file
+        upload_response = requests.post(response.json()['url'], data=payload, headers=headers)
 
 
+        #Step 5: If the upload is successful, register the upload
+        if upload_response.status_code == 201:
+            print("File successfully uploaded")
 
-print("step three done") #The step to confirm upload ability
+            register_headers = {
+                # Because the content to be sent is not application/json, you use data, the generic way to send post data
+                "Content-Type": "application/x-www-form-urlencoded",
+                "If-None-Match": "*",
+                "Zotero-API-Version": "3",
+                "Zotero-API-Key": self.api_key
+            }
+
+            register_data = {
+                "upload": response.json()['uploadKey']
+            }
+
+            register_response = requests.post(
+                f"https://api.zotero.org/users/{self.userID}/items/{jsonToGetKey['successful']['0']['key']}/file",
+                data=register_data, headers=register_headers)
+            if register_response.status_code == 204:
+                print("Upload successfully registered")
+            else:
+                print("Error registering upload:", register_response.text)
+        else:
+            print("Error uploading file:", upload_response.text)
+
+    def uploadExisting(self, pdf_key: str, filename: str):
+        pass
+
+    def getAPIrequest(self, link: str) -> requests.models.Response:
+        headers = {
+            "Zotero-API-Version": "3",
+            "Zotero-API-Key": self.api_key,
+
+        }
+        try:
+            response = requests.get(link, headers=headers)
+            if response.status_code == 200:
+                return response
+            else:
+                print(f"API call failed. Status code: {response.status_code}. ")
+                exit()
+        except Exception as e:
+            print(f"API request error: {e}")
+
+if __name__ == "__main__":
+    #Keeping the trailing slash int he directory address
+    zotero = uploadZoteroAPI(userID = "", api_key = "", directory_address = "", file_name = "")
+    zotero.upload()
 
 
-#Last step: to actually upload the file:
-
-# ... [Your code above]
-
-# STEP 4: UPLOAD THE FILE
-headers = {
-    "Content-Type": response.json()['contentType'],  # this should be set based on the API response from Step 3
-    "Zotero-API-Version": "3",
-    "Zotero-API-Key": api_key
-}
-
-# Concatenate prefix, file content, and suffix
-with open(f"/Users/phillipyan/Downloads/{jsonToGetKey['successful']['0']['data']['title']}", "rb") as file:
-    file_content = file.read()
-
-# Convert prefix and suffix to bytes
-prefix_bytes = response.json()['prefix'].encode('utf-8')
-suffix_bytes = response.json()['suffix'].encode('utf-8')
-
-# Now concatenate
-payload = prefix_bytes + file_content + suffix_bytes
-
-# Upload the file
-
-upload_response = requests.post(response.json()['url'], data=payload, headers=headers)
-
-if upload_response.status_code == 201:
-    print("File successfully uploaded")
-
-    # STEP 5: REGISTER THE UPLOAD
-    register_headers = {
-        #Because the content to be sent is not application/json, you use data, the generic way to send post data
-        "Content-Type": "application/x-www-form-urlencoded",
-        #Figure out why I need this?
-        "If-None-Match": "*",
-
-        # I don't think I need the code below
-        "Zotero-API-Version": "3",
-        "Zotero-API-Key": api_key
-    }
-    register_data = {
-        "upload": response.json()['uploadKey']
-    }
-    register_response = requests.post(f"https://api.zotero.org/users/{userID}/items/{jsonToGetKey['successful']['0']['key']}/file", data=register_data, headers=register_headers)
-    if register_response.status_code == 204:
-        print("Upload successfully registered")
-    else:
-        print("Error registering upload:", register_response.text)
-else:
-    print("Error uploading file:", upload_response.text)
 
